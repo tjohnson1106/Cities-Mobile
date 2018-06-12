355fccc56bade6f079c65ac260dc9b11e64ab5f4
Thomas Johnson <tjohnson1106@gmail.com>
2018-06-12 14:58:29 -0400

    created add location and on change text function in City component

--- src/Cities/City.js
+++ src/Cities/City.js
@@ -1,8 +1,52 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Textinput,
+  ScrollView,
+  TouchableWithoutFeedback,
+  TouchableOpacity
+} from "react-native";
+
+import { colors } from "../theme";
 
 class City extends Component {
-  state = {};
+  static navigationOptions = props => {
+    return {
+      title: props.navigation.state.params.city.city,
+      headerTitleStyle: {
+        fontSize: 20,
+        fontWeight: "400"
+      }
+    };
+  };
+
+  state = {
+    name: "",
+    info: ""
+  };
+
+  onChangeText = (key, value) => {
+    this.setState({
+      [key]: value
+    });
+  };
+
+  addLocation = () => {
+    if (this.state.name === "" || this.state.info === "") return;
+    const { city } = this.props.navigation.state.params;
+    const location = {
+      name: this.state.name,
+      info: this.state.info
+    };
+    this.props.screenProps.addLocation(location, city);
+    this.setState({
+      name: "",
+      info: ""
+    });
+  };
+
   render() {
     return (
       <View>
